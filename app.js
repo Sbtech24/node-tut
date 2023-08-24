51264917a313cca4ebd11085ed7d45795095778d
sbtech24 <bajomosemilore@gmail.com>
2023-08-25 00:58:09 +0100

    lodash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const _ = require('lodash')
-
-const items = [1,[2,[3,[4]]]]
-
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+// started operating system process
+console.log('first')
+setTimeout(() => {
+  console.log('second')
+}, 0)
+console.log('third')
+// completed and exited operating system process
